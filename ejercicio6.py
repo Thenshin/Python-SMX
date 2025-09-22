05fdd69138239372a0337524cc9ccf9969888e1a
Aleksander Petrov Kodzhaboyukliev <alexpetrov18372@gmail.com>
2025-09-22 11:51:33 +0200

    Add files via upload

---
+++ ejercicio6.py
@@ -0,0 +1,9 @@
+edad=int(input("Ingrese su edad: "))
+
+if edad <0 or edad >120:
+        print("eres un edad no valida")
+elif edad >=18:
+    print("eres mayor de edad")
+else :
+        print("eres menor de edad")
+
