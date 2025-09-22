d258ef39d1638735749933e8657b69df820eba0b
Aleksander Petrov Kodzhaboyukliev <alexpetrov18372@gmail.com>
2025-09-22 11:36:17 +0200

    Add files via upload

---
+++ ejercicio5.py
@@ -0,0 +1,8 @@
+numero=int(input("Ingrese un numero: "))
+#Si el numero es divisible entre 2 es par, sino impar
+if numero % 2==0:
+    print("El numero es par y su valor es: ",numero)
+else:
+    print("El numero es impar y su valor es: ",numero)     
+
+
